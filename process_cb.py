6f531dd123e7a7ca4b87cf5efebe5e19f04f61ab
Jake Hughes <jh@jakehughes.uk>
2023-02-22 21:08:16 +0000

    Add geometric mean to end of table

--- process_cb.py
+++ process_cb.py
@@ -7,37 +7,46 @@ from statistics import geometric_mean, stdev
 
 def process_speedometer(results_file, pexecs):
     results = {}
+    geomeans = {}
 
-    unwanted = ['score']
+    wanted = ['total', 'geomean']
     with open(results_file) as f:
         data = json.load(f)
         for browser_name, browser in data.items():
             for bm, runs in browser['data'].items():
                 # We only want the totals
-                if len(bm.split('/')) > 2 or any([x in bm for x in unwanted]):
+                if len(bm.split('/')) > 2 or not any([x in bm for x in wanted]):
                     continue
 
-                bm_name = bm if bm != "mean" else "geometric mean"
-                if bm_name not in results:
-                    results[bm_name] = {}
-                results[bm_name][browser_name] = {
+                if bm == "geomean":
+                    geomeans[browser_name] = runs['geomean']
+                    continue
+
+                if bm not in results:
+                    results[bm] = {}
+
+                results[bm][browser_name] = {
                     'mean' : runs['average'],
                     'geomean' : runs['geomean'],
                     'stddev' : runs['stddev'],
                 }
 
+    gen_table(results, geomeans, pexecs)
 
+
+def gen_table(results, geomeans, pexecs):
     with open("table_no_gc.tex", "w") as f:
         for bm, browsers in results.items():
             f.write("%s " % bm)
             for b, data in browsers.items():
-                if "mean" in bm:
-                    f.write("& %.3f " % data['geomean'])
-                else:
-                    f.write("& %.3f ± %.6f " % \
-                            (data['mean'], \
-                            confidence_interval(data['mean'], data['stddev'], pexecs)))
+                f.write("& %.3f ± %.6f " % \
+                        (data['mean'], \
+                        confidence_interval(data['mean'], data['stddev'], pexecs)))
             f.write(" \\\\\n")
+        f.write("geometric mean ")
+        for browser, gmean in geomeans.items():
+            f.write("& %.3f " % gmean)
+        f.write(" \\\\\n")
 
 def confidence_interval(mean, stddev, num_samples):
     Z = 2.576  # 99% interval

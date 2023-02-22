a4cd65834b8f4277bf3aacc075c6b15142476a13
Jake Hughes <jh@jakehughes.uk>
2023-02-22 19:27:22 +0000

    Add script for processing crossbench result files

---
+++ process_cb.py
@@ -0,0 +1,42 @@
+import json
+import sys
+import pprint
+import math
+import os
+from statistics import geometric_mean, stdev
+
+def process_speedometer(results_file, pexecs):
+    results = {}
+    with open(results_file) as f:
+        data = json.load(f)
+        for browser_name, browser in data.items():
+            for bm, runs in browser['data'].items():
+                # We only want the totals
+                if len(bm.split('/')) > 2 or "total" not in bm:
+                    continue
+                if bm not in results:
+                    results[bm] = {}
+                results[bm][browser_name] = {
+                    'mean' : runs['average'],
+                    'stddev' : runs['stddev'],
+                }
+
+
+    with open("table_no_gc.tex", "w") as f:
+        for bm, browsers in results.items():
+            f.write("%s " % bm)
+            for b, data in browsers.items():
+                f.write("& %.3f ± %.6f " % \
+                        (data['mean'], \
+                        confidence_interval(data['mean'], data['stddev'], pexecs)))
+            f.write(" \\\\\n")
+
+def confidence_interval(mean, stddev, num_samples):
+    Z = 2.576  # 99% interval
+    return Z * (stddev / math.sqrt(num_samples))
+
+if __name__ == "__main__":
+    results = "crossbench_no_gc/speedometer_2.1.json"
+    process_speedometer(results, 10)
+
+
